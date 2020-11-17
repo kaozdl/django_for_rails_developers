70ad9bdc2d74972dd7caf4a285ed83902d86ee20
Kalil de Lima <kalil@rootstrap.com>
2020-11-17 20:47:34 -0300

    create board viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from trello import models
+from api import serializers
 # Create your views here.
+class BoardViewSet(viewsets.ModelViewSet):
+
+    queryset = models.Board.objects.all()
+    serializer_class = serializers.BoardSerializer
